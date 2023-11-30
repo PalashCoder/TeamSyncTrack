972314600c870f88fb36c298476b42527b43c3b8
Palash Das <ng.palash.das@gmail.com>
2023-11-30 22:38:24 +0530

    landing page 1st attempt

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <section>
+            <div
+                className='overflow-hidden 
+                px-4 
+                sm:px-6 
+                mt-10 
+                sm:flex 
+                sm:flex-col
+                gap-4 
+                md:justify-center
+                md:items-center'>
+                HomePage
+            </div>
+        </section>
+    )
+}
+
+export default HomePage
